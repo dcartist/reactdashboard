547bdcb9e3a7f20696c02b1f2acb3a5dbff97e54
dcartist <pbannerman13@gmail.com>
2022-11-14 00:10:06 -0500

    react vs react native

--- src/pages/home/homeTopInformation.js
+++ src/pages/home/homeTopInformation.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function HomeTopInformation() {
-  return <div></div>;
+  return (
+    <div>
+      <p>
+        ReactJS is a JavaScript library, supporting both front-end web and being
+        run on a server, for building user interfaces and web applications. It
+        follows the concept of reusable components.
+      </p>
+      <p>
+        React Native is a mobile framework that makes use of the JavaScript
+        engine available on the host, allowing you to build mobile applications
+        for different platforms (iOS, Android, and Windows Mobile) in JavaScript
+        that allows you to use ReactJS to build reusable components and
+        communicate with native components further explanation
+      </p>
+    </div>
+  );
 }
